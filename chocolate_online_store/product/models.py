76f7e1ce81a09891c4b6a6586f5c19902ab2295d
Mariam <mari.kifshidze@gmail.com>
2023-11-29 13:17:29 +0400

    Create Product model

--- chocolate_online_store/product/models.py
+++ chocolate_online_store/product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from ckeditor_uploader.fields import RichTextUploadingField
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(verbose_name=_('title'), max_length=255)
+    ingredients = RichTextUploadingField(verbose_name=_('Ingredients'))
+    price = models.DecimalField(verbose_name=_('Price'), max_digits=6, decimal_places=2)
+
+    class Meta:
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
