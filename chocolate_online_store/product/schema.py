3a681300787d90306665efc804523b1c7fda8b18
Mariam <mari.kifshidze@gmail.com>
2023-11-30 17:29:58 +0400

    Add request format

--- chocolate_online_store/product/schema.py
+++ chocolate_online_store/product/schema.py
@@ -21,6 +21,18 @@ class Query(graphene.ObjectType):
     all_categories = graphene.List(CategoryType)
     get_product = graphene.Field(ProductType, product_id=graphene.Int())
 
+    """
+    Request Format:
+    {
+      allProducts{
+        id
+        category{
+          id 
+          title
+        }
+      }
+    }
+    """
     @staticmethod
     def resolve_all_products(*_, **__):
         return Product.objects.all()
@@ -29,6 +41,18 @@ class Query(graphene.ObjectType):
     def resolve_all_categories(*_, **__):
         return Category.objects.all()
 
+    """
+    Request Format:
+    {
+      getProduct(productId:1){
+        id
+        category{
+          id 
+          title
+        }
+      }
+    }
+    """
     def resolve_get_product(self, _, product_id: int):
         return Product.objects.get(pk=product_id)
 
